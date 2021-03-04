7424c2f6615e3dacbf1cc5b6b23392be95816738
Asghar Nazir <asghar@syntaxtech.com>
2021-03-04 14:30:31 -0500

    2nd

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Enter 2 numbers");
-        Scanner scanner=new Scanner(System.in);
-        int num1=scanner.nextInt();
-        int num2=scanner.nextInt();
-        System.out.println("Result is "+add(num1,num2));
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Runnning");
+            Thread.sleep(2000);
+        }
     }
-    public static int add(int num1,int num2){
-        return num1+num2;
-    }
+
 }
